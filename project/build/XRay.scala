2f0a963c4a9ae5a4f8e9d7ce2ec17064f9c538b3
Mark Harrah <dmharrah@gmail.com>
2009-06-25 22:22:25 -0400

    Updated build for sbt 0.5

--- project/build/XRay.scala
+++ project/build/XRay.scala
@@ -1,23 +1,12 @@
 import sbt._
 
 class XRay(info: ProjectInfo) extends DefaultProject(info) {
-  val resources_managed = path("src_managed") / "main" / "resources"
   val jquery_version = "1.3.2"
-  val jquery_js = resources_managed / ("jquery-" + jquery_version + ".min.js")
-  
-  lazy val jquery = fileTask(jquery_js :: Nil) {
-    import FileUtilities._
-    val remote_js = new java.net.URL("http://jqueryjs.googlecode.com/files/jquery-" + jquery_version + ".min.js")
-    
-    readStream(remote_js, log) { inputStream =>
-      writeStream(jquery_js.asFile, log) { outputStream =>
-        transfer(inputStream, outputStream, log)
-      }
-    }
-  }
-  override def mainResources = super.mainResources +++ descendents(resources_managed ##, "*")
-  override def packageAction = super.packageAction dependsOn jquery
+  val js = config("js") hide
+  val jquery = "jquery" % "jquery" % jquery_version % "js->default" from ("http://jqueryjs.googlecode.com/files/jquery-" + jquery_version + ".min.js")
+  override def mainResources = super.mainResources +++ descendents(configurationPath(js) ##, "*.js")
 
   override def crossScalaVersions = Set("2.7.2", "2.7.3", "2.7.4", "2.7.5")
-
+  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+  Credentials(Path.userHome / ".ivy2" / ".credentials", log)
 }
