64c52213e34ad3e53a1591e189ee2e0962f727c9
Mark Harrah <dmharrah@gmail.com>
2013-04-08 08:41:44 -0400

    fix parameter name in doc comment

--- src/main/scala/Browse.scala
+++ src/main/scala/Browse.scala
@@ -370,7 +370,7 @@ abstract class Browse extends Plugin
 		}
 	}
 
-	/** Generates a link usable in the file 'from' to the symbol 'to', which might be in some other file. */
+	/** Generates a link usable in the file 'from' to the symbol 'sym', which might be in some other file. */
 	private def linkTo(from: File, sym: Symbol, links: LinkMap): Option[Link] =
 	{
 		if(sym == null || sym == NoSymbol || sym.owner == NoSymbol)
