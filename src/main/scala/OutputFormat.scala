e4d7bc004efc9326896e3f6cbaf0e19b2fbf12c3
Mark Harrah <dmharrah@gmail.com>
2013-09-23 12:19:28 -0400

    OutputFormat.scala line endings

--- src/main/scala/OutputFormat.scala
+++ src/main/scala/OutputFormat.scala
@@ -12,13 +12,11 @@ object OutputFormat extends Enumeration {
 
 	// The enumeration values
 	val Html = Value("html")
-	val Vim = Value("vim")
-	def all: List[OutputFormat] = Html :: Vim :: Nil
+	def all: List[OutputFormat] = Html :: Nil
 
 	private[this] type Factory = OutputWriterContext => OutputWriter
 	private[this] def factory(format: OutputFormat): Factory = format match {
 		case Html => new HtmlWriter(_)
-		case Vim => new vim.VimWriter(_)
 	}
 
 	/** Returns the writer corresponding to a value, configured with a context */
