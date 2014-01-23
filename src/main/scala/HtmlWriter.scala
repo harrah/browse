de9fc7901863f6aa055f039ed992d8610eb7da7c
Rory Graves <rory.graves@fieldmark.co.uk>
2014-01-23 21:47:17 +0000

    Update versions of jQuery/qtip and improve html display

--- src/main/scala/HtmlWriter.scala
+++ src/main/scala/HtmlWriter.scala
@@ -11,27 +11,20 @@ object HtmlWriter
 	/** The location to store the index relative to the output directory.*/
 	val IndexRelativePath = "index.html"
 	/** The location to store the style sheet relative to the output directory.*/
-	val CSSRelativePath = "style.css"
+	val StyleCssRelativePath = "style.css"
+
 	/** The location to store the script relative to the output directory.*/
-	val JSRelativePath = "linked.js"
+	val LinkedJsRelativePath = "linked.js"
+
 	/** The location to store jQuery relative to the output directory.*/
-	val JQueryRelativePath = "jquery-all.js"
+	val JQueryJsRelativePath = "jquery-1.10.2.js"
+
+	val JQueryScrollToJsRelativePath = "jquery-scrollto-1.4.9.js"
+
+	val JQueryQTip2JsRelativePath = "jquery-qtip2-2.2.0.js"
+	val JQueryQTip2CssRelativePath = "jquery-qtip2-css-2.2.0.css"
 
 	val HtmlExtension = ".html"
-
-	/** The path of the default style sheet resource.*/
-	val DefaultCSS = "/default-style.css"
-	/** The path of the default script resource.*/
-	val LinkedJS = "/linked.js"
-	/** The path of the JQuery resource.*/
-	val LinkedJQuery = "/" + JQueryRelativePath
-
-	/** Copies the default style sheet available as a resource on the classpath to the file 'to'.*/
-	def writeDefaultCSS(to: File) { FileUtil.writeResource(DefaultCSS, to) }
-	/** Copies the default script available as a resource on the classpath to the file 'to'.*/
-	def writeJS(to: File) { FileUtil.writeResource(LinkedJS, to) }
-	/** Copies the jQuery script available as a resource on the classpath to the file 'to'.*/
-	def writeJQuery(to: File) { FileUtil.writeResource(LinkedJQuery, to) }
 
 	final val nl = System.getProperty("line.separator")
 }
@@ -47,16 +40,24 @@ class HtmlWriter(context: OutputWriterContext) extends OutputWriter {
 	val info = new OutputInfo(outputDirectory, HtmlExtension)
 
 	import info._
-	val cssFile = new File(outputDirectory, CSSRelativePath)
-	val jsFile = new File(outputDirectory, JSRelativePath)
-	val jQueryFile = new File(outputDirectory, JQueryRelativePath)
 
 	private var outputFiles = List[File]()
 
+	val styleCssFile = new File(outputDirectory, StyleCssRelativePath)
+	val linkedJsFile = new File(outputDirectory, LinkedJsRelativePath)
+	val jQueryFile = new File(outputDirectory, JQueryJsRelativePath)
+	val jQueryScrollToFile = new File(outputDirectory, JQueryScrollToJsRelativePath)
+	val jQueryQTip2JsFile = new File(outputDirectory, JQueryQTip2JsRelativePath)
+	val jQueryQTip2CssFile = new File(outputDirectory, JQueryQTip2CssRelativePath)
+
 	def writeStart() {
-		writeDefaultCSS(cssFile)
-		writeJS(jsFile)
-		writeJQuery(jQueryFile)
+		/** The location to store the style sheet relative to the output directory.*/
+		FileUtil.writeResource(StyleCssRelativePath,styleCssFile)
+		FileUtil.writeResource(LinkedJsRelativePath,linkedJsFile)
+		FileUtil.writeResource(JQueryJsRelativePath,jQueryFile)
+		FileUtil.writeResource(JQueryScrollToJsRelativePath,jQueryScrollToFile)
+		FileUtil.writeResource(JQueryQTip2JsRelativePath,jQueryQTip2JsFile)
+		FileUtil.writeResource(JQueryQTip2CssRelativePath,jQueryQTip2CssFile)
 	}
 
 	def writeUnit(sourceFile: File, relativeSourcePath: String, tokenList: List[Token]) {
@@ -64,7 +65,14 @@ class HtmlWriter(context: OutputWriterContext) extends OutputWriter {
 		outputFiles ::= outputFile
 		def relPath(f: File) = FileUtil.relativePath(outputFile, f)
 
-		val styler = new BasicStyler(relativeSourcePath, relPath(cssFile), relPath(jsFile),  relPath(jQueryFile))
+		val styler = new BasicStyler(relativeSourcePath,
+      relPath(styleCssFile),
+      relPath(linkedJsFile),
+      relPath(jQueryFile),
+      relPath(jQueryScrollToFile),
+      relPath(jQueryQTip2JsFile),
+      relPath(jQueryQTip2CssFile))
+
 		Annotate(sourceFile, encoding, outputFile, tokenList, styler)
 	}
 
@@ -78,25 +86,39 @@ class HtmlWriter(context: OutputWriterContext) extends OutputWriter {
 		val sortedRelativePaths = wrap.Wrappers.treeSet[String]
 		sortedRelativePaths ++= rawRelativePaths
 		FileUtil.withWriter(to) { out =>
-			out.write("<html><body>")
+			out.write(
+        s"""<html>
+            |  <head>
+            |    <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+            |    <link rel="stylesheet" type="text/css" href="$StyleCssRelativePath" title="Style"/>
+            |  </head>
+            |  <body>
+            |    <div id="index">
+            |      <ul>
+            |""".stripMargin)
 			sortedRelativePaths.foreach(writeEntry(to, out))
-			out.write(nl)
-			out.write("</body></html>")
+      out.write(
+        """      </ul>
+          |    </div>
+          |    <div id="content">
+          |      <iframe name="target" id="target">
+          |      </iframe>
+          |    </div>
+          |  </body>
+          |</html>
+          |""".stripMargin)
 		}
 	}
 	import java.io.Writer
 	private def writeEntry(index: File, out: Writer)(path: String)
 	{
-		out.write(nl)
-		out.write("<li><a href=\"")
-		out.write(path)
-		out.write("\">")
-		val label =
-			if(path.endsWith(".html"))
-				path.substring(0, path.length - ".html".length)
-			else
-				path
-		out.write(label)
-		out.write("</a></li>")
+    val label =
+      if(path.endsWith(".html"))
+        path.substring(0, path.length - ".html".length)
+      else
+        path
+
+    out.write( s"""        <li><a target="target" href="$path">$label</a></li>""")
+    out.write(nl)
 	}
 }
